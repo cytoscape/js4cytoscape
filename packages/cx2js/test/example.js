695e17e9026d1ee9adf17a5b124d28080ef9f1ea
Unknown <dotasek.dev@gmail.com>
2018-08-13 12:57:12 -0700

    Initial commit

---
+++ packages/cx2js/test/example.js
@@ -0,0 +1,13 @@
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+
+describe('Example', function(){
+  it('does something', function(){
+    expect( 1 ).to.equal( 1 );
+  });
+
+  it('does something else', function(){
+    expect( 2 + 2 ).to.equal( 4 );
+  });
+});
