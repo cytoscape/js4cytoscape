85758b90da4ff8340c4b3764a3211fd460bd37a3
Unknown <dotasek.dev@gmail.com>
2018-08-14 11:41:21 -0700

    Added requirement for test

--- packages/cx2js/test/example.js
+++ packages/cx2js/test/example.js
@@ -1,9 +1,11 @@
 var chai = require('chai');
+var cytoscapeCx2js = require('../build/bundle.js');
 var expect = chai.expect;
 var assert = chai.assert;
 
 describe('Example', function(){
-  it('does something', function(){
+  it('initialization', function(){
+    var utils = new cytoscapeCx2js.cyNetworkUtils();
     expect( 1 ).to.equal( 1 );
   });
 
