54c28b4710d800ddabb0b5d5e569df116daffa03
Unknown <dotasek.dev@gmail.com>
2018-11-19 11:26:02 -0800

    First canvas test

--- packages/js4cytoscape/packages/cx2js/test/cx_to_cy_canvas_tests.js
+++ packages/js4cytoscape/packages/cx2js/test/cx_to_cy_canvas_tests.js
@@ -1,41 +1,55 @@
 const { expect, assert } = require('chai');
-const { CxToCyCanvas, CyNetworkUtils } = require('../src');
-const fs  = require('fs-extra');
+const { CxToCyCanvas } = require('../src');
 const sinon = require("sinon");
-
 
 describe('CX to Cytoscape JS Canvas', function(){
   
-  it('first', function(){
+  it('empty CX', function(){
     var cx2canvas = new CxToCyCanvas();
 
     let cytoscape  = sinon.spy();
 
-    var canvasSpy = {
-        getContext : sinon.spy()
+    let ctxSpy = {
+        save : sinon.spy(),
+        restore : sinon.spy()
     };
-    //sinon.stub(canvasSpy, "cyCanvas").returns(layerSpy);
+
+    var canvasSpy = {
+        getContext : function(){}
+    };
+
+    sinon.stub(canvasSpy, "getContext").returns(ctxSpy);
 
     var layerSpy = {
-        getCanvas: function(){}
+        getCanvas: function(){},
+        resetTransform : sinon.spy(),
+        setTransform: sinon.spy(),
+        clear: sinon.spy()
     };
 
     sinon.stub(layerSpy, "getCanvas").returns(canvasSpy);
 
-    let cy = {
-        cyCanvas: function(){},
-        on: function(){}
-    };
-    sinon.stub(cy, "cyCanvas").returns(layerSpy);
+    var resizeFunction = {};
 
+    let cytoscapeInstance = {
+        cyCanvas: function(){},
+        on: function(eventName, f) { console.log(eventName); resizeFunction = f;}
+    };
+    sinon.stub(cytoscapeInstance, "cyCanvas").returns(layerSpy);
+   
 
     let niceCX = {
 
     };
 
-    cx2canvas.drawAnnotationsFromNiceCX(cytoscape, cy, niceCX);
+    cx2canvas.drawAnnotationsFromNiceCX(cytoscape, cytoscapeInstance, niceCX);
+    
+    resizeFunction();
+    
+    expect( cytoscape.called ).to.eql( true );
 
-    expect( true ).to.eql( true );
+    console.log(layerSpy.resetTransform);
+
   });
   
   
