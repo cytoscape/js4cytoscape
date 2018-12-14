88f25bcbd2d94fe2d7f270f9d365154bf719e313
Unknown <dotasek.dev@gmail.com>
2018-12-13 16:07:34 -0800

    Edit merge from issues/15

--- packages/cx2js/src/index.js
+++ packages/cx2js/src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var CxToJs = require('./cx_to_js.js').CxToJs;
-var CxToCyCanvas = require('./cx_to_cy_canvas.js').CxToCyCanvas;
 var CyNetworkUtils = require ('./cy_network_utils.js').CyNetworkUtils;
 
-module.exports = { CxToJs, CyNetworkUtils, CxToCyCanvas};
+module.exports = { CxToJs, CyNetworkUtils};
