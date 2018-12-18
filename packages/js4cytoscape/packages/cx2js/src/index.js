03eae3b38c5fc59253e7ebdc2685fcf099980393
Unknown <dotasek.dev@gmail.com>
2018-12-18 12:31:39 -0800

    Export constants for consuming libraries

--- packages/js4cytoscape/packages/cx2js/src/index.js
+++ packages/js4cytoscape/packages/cx2js/src/index.js
@@ -3,4 +3,7 @@
 var CxToJs = require('./cx_to_js.js').CxToJs;
 var CyNetworkUtils = require ('./cy_network_utils.js').CyNetworkUtils;
 
-module.exports = { CxToJs, CyNetworkUtils};
+const JavaLogicalFontConstants =require('./java_logical_font_constants.js');
+const CommonOSFontConstants =require('./common_os_font_constants.js');
+
+module.exports = { CxToJs, CyNetworkUtils, CxToCyCanvas, JavaLogicalFontConstants, CommonOSFontConstants};
