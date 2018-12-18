984f88ad6a6db61828603a96266ddcb144829fd9
Unknown <dotasek.dev@gmail.com>
2018-12-18 12:45:44 -0800

    Remove orphaned CxToCyCanvas module

--- packages/cx2js/src/index.js
+++ packages/cx2js/src/index.js
@@ -6,4 +6,4 @@ var CyNetworkUtils = require ('./cy_network_utils.js').CyNetworkUtils;
 const JavaLogicalFontConstants =require('./java_logical_font_constants.js');
 const CommonOSFontConstants =require('./common_os_font_constants.js');
 
-module.exports = { CxToJs, CyNetworkUtils, CxToCyCanvas, JavaLogicalFontConstants, CommonOSFontConstants};
+module.exports = { CxToJs, CyNetworkUtils, JavaLogicalFontConstants, CommonOSFontConstants};
