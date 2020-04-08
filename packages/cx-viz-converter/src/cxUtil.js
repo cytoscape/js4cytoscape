d13e48f2711a2e9274169869f7dc51f49e45fa4e
D Otasek <dotasek.dev@gmail.com>
2020-04-08 12:14:23 -0400

    Refactor + start large network conversion

--- packages/cx-viz-converter/src/cxUtil.js
+++ packages/cx-viz-converter/src/cxUtil.js
@@ -15,7 +15,73 @@ function getCxMajorVersion(versionString) {
     return versionString ? getCxVersion(versionString)[0] : 1;
 }
 
+function processAttributeDeclarations(cxAttributeDeclarations, 
+    nodeAttributeNameMap, 
+    nodeAttributeTypeMap, 
+    nodeAttributeDefaultValueMap, 
+    edgeAttributeNameMap, edgeAttributeTypeMap, 
+    edgeAttributeDefaultValueMap) {
+    console.log(" cxAttributeDeclarations: " + JSON.stringify(cxAttributeDeclarations, null, 2));
+    cxAttributeDeclarations.forEach((cxAttributeDeclaration) => {
+        if (cxAttributeDeclaration['nodes']) {
+            updateAttributeNameMap(nodeAttributeNameMap, cxAttributeDeclaration.nodes);
+            updateAttributeTypeMap(nodeAttributeTypeMap, cxAttributeDeclaration.nodes);
+            updateAttributeDefaultValueMap(nodeAttributeDefaultValueMap, cxAttributeDeclaration.nodes);
+        }
+        if (cxAttributeDeclaration['edges']) {
+            updateAttributeNameMap(edgeAttributeNameMap, cxAttributeDeclaration.edges);
+            updateAttributeTypeMap(edgeAttributeTypeMap, cxAttributeDeclaration.edges);
+            updateAttributeDefaultValueMap(edgeAttributeDefaultValueMap, cxAttributeDeclaration.edges);
+        }
+    });
+}
+
+function updateAttributeTypeMap(attributeTypeMap, attributeDeclarations) {
+    Object.keys(attributeDeclarations).forEach((attributeName) => {
+        const attributeDeclaration = attributeDeclarations[attributeName];
+        if (attributeDeclaration['d']) {
+            attributeTypeMap.set(attributeName, attributeDeclaration.d);
+        }
+    });
+}
+
+function updateAttributeNameMap(attributeNameMap, attributeDeclarations) {
+    Object.keys(attributeDeclarations).forEach((attributeName) => {
+        const attributeDeclaration = attributeDeclarations[attributeName];
+        if (attributeDeclaration['a']) {
+            console.log('attribute ' + attributeDeclaration.a + ' should be renamed to ' + attributeName);
+            attributeNameMap.set(attributeDeclaration.a, attributeName);
+        }
+    });
+}
+
+function updateAttributeDefaultValueMap(attributeDefaultValueMap, attributeDeclarations) {
+    Object.keys(attributeDeclarations).forEach((attributeName) => {
+        const attributeDeclaration = attributeDeclarations[attributeName];
+        if (attributeDeclaration['v']) {
+            console.log('attribute ' + attributeName + ' has default value ' + attributeDeclaration.v);
+            attributeDefaultValueMap.set(attributeName, attributeDeclaration.v);
+        }
+    });
+}
+
+function updateInferredTypes(attributeTypeMap, attributeNameMap, v) {
+    Object.keys(v).forEach((key) => {
+        if (!attributeTypeMap.has(key)) {
+            const value = v[key];
+            const inferredType = typeof value;
+            const newKey = attributeNameMap.has(key) ? attributeNameMap.get(key) : key;
+            attributeTypeMap.set(newKey, inferredType);
+        }
+    })
+}
+
 module.exports = {
     getCxVersion: getCxVersion,
-    getCxMajorVersion: getCxMajorVersion
+    getCxMajorVersion: getCxMajorVersion,
+    processAttributeDeclarations: processAttributeDeclarations,
+    updateAttributeTypeMap: updateAttributeTypeMap,
+    updateAttributeNameMap: updateAttributeNameMap,
+    updateAttributeDefaultValueMap: updateAttributeDefaultValueMap,
+    updateInferredTypes: updateInferredTypes
 };
--- packages/cx-viz-converter/src/largeNetwork/largeNetwork.js
+++ packages/cx-viz-converter/src/largeNetwork/largeNetwork.js
@@ -1,45 +1,86 @@
 
 const cxConstants = require('../cxConstants.js');
+const largeNetworkConstants = require('./largeNetworkConstants.js');
 const cxUtil = require('../cxUtil.js');
 
 function lnvConvert(cx) {
-    let output = {};
-
+    
     //First pass. 
     // We may need to collect object attributes to calculate
     // mappings in the second pass. 
-    let nodeValueMap = {};
-    let nodeLayoutMap = {};
-    let cxNodes;
-    let cxEdges;
+    let nodeMap = new Map();
+    let edgeMap = new Map();
+
     let cxVisualProperties;
-    cx.forEach((element) => {
-        if (element['nodes']) {
-            cxNodes = element['nodes'];
+
+    let nodeAttributeTypeMap = new Map();
+    let edgeAttributeTypeMap = new Map();
+
+    let nodeAttributeNameMap = new Map();
+    let edgeAttributeNameMap = new Map();
+
+    let nodeAttributeDefaultValueMap = new Map();
+    let edgeAttributeDefaultValueMap = new Map();
+
+    cx.forEach((cxAspect) => {
+        if (cxAspect['attributeDeclarations']) {
+            const cxAttributeDeclarations = cxAspect['attributeDeclarations'];
+            console.log(" cxAttributeDeclarations: " + JSON.stringify(cxAttributeDeclarations, null, 2));
+            cxUtil.processAttributeDeclarations(cxAttributeDeclarations,
+                nodeAttributeNameMap,
+                nodeAttributeTypeMap,
+                nodeAttributeDefaultValueMap,
+                edgeAttributeNameMap,
+                edgeAttributeTypeMap,
+                edgeAttributeDefaultValueMap
+                );
+        } else if (cxAspect['nodes']) {
+            const cxNodes = cxAspect['nodes'];
             cxNodes.forEach((cxNode) => {
-                const cxId = cxNode['id'].toString();
-                nodeValueMap[cxId] = cxNode['v'];
-                nodeLayoutMap[cxId] = {
-                    x: cxNode['x'],
-                    y: cxNode['y'],
-                    z: cxNode['z']
+                const cxId = cxNode[cxConstants.ID].toString();
+                const node = {
+                    id : cxId,
+                    position: cxNode['z'] ? 
+                        [cxNode['x'],cxNode['y'],cxNode['z']] 
+                        : [cxNode['x'],cxNode['y']]
                 }
+                nodeMap.set(cxId, node);
             })
-        } else if (element['edges']) {
-            cxEdges = element['edges'];
-        } else if (element['visualProperties']) {
-            cxVisualProperties = element['visualProperties'];
+        } else if (cxAspect['edges']) {
+            const cxEdges = cxAspect['edges'];
+            cxEdges.forEach((cxEdge) => {
+                const cxId = cxEdge[cxConstants.ID].toString();
+                const edge = {
+                    id : cxId,
+                    s : cxEdge.s.toString(),
+                    t : cxEdge.t.toString() 
+                }
+                edgeMap.set(cxId, edge);
+            })
+        } else if (cxAspect['visualProperties']) {
+            cxVisualProperties = cxAspect['visualProperties'];
         }
     });
 
-    console.log("attributes: " + JSON.stringify(nodeValueMap));
-    console.log("layout: " + JSON.stringify(nodeLayoutMap));
+    let output = {};
+    
+    let nodeViews = [];
+    let edgeViews = [];
+
 
     //Second pass. 
     // Here is where the actual output is generated.
-    output.nodeCount = cxNodes.length;
-    output.edgeCount = cxEdges.length;
-    output.visualPropertyCount = cxVisualProperties.length;
+    
+    nodeMap.forEach((value, id) => {
+        nodeViews.push(value);
+    });
+
+    edgeMap.forEach((value, id) => {
+        edgeViews.push(value);
+    });
+    
+    output[largeNetworkConstants.nodeViews] = nodeViews;
+    output[largeNetworkConstants.edgeViews] = edgeViews;
 
     return output;
 }
