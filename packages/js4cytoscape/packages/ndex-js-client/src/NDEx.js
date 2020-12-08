ce7da73422a1eb0f0c30cbaeec3af93398d98143
Jing Chen <jic002@ucsd.edu>
2020-12-08 15:52:01 -0700

    UD-1552. Adding edge filter and get aspect element functions.

--- packages/js4cytoscape/packages/ndex-js-client/src/NDEx.js
+++ packages/js4cytoscape/packages/ndex-js-client/src/NDEx.js
@@ -5,23 +5,23 @@ class NDEx {
       if (hostprefix === undefined || hostprefix === null || hostprefix === '') {
         throw new Error('NDEx server endpoint base URL is required in client constructor.');
       }
-  
+
       this._host = hostprefix;
-     
+      this._v3baseURL = this._host.substring(0,this.host.lastIndexOf('v2')) + 'v3';
     }
     get host() { return this._host;}
-  
+
     get googleAuth() {
       return this._googleAuth;
     }
-  
+
     SetGoogleAuth(googleAuthObj) {
       if (googleAuthObj !== undefined) {
         this._googleAuth = googleAuthObj;
         this._authType = 'g'; // valid values are 'g','b' or undefined
       }
     }
-  
+
     setAuthToken(authToken) {
       if (authToken !== undefined ) {
         this._authToken = authToken;
@@ -32,14 +32,14 @@ class NDEx {
     get authenticationType() {
       return this._authType;
     }
-  
+
     get username() {
       return this._username;
     }
-  
+
     // get authStr() { return this._authStr;}
     get password() { return this._password;}
-  
+
     setBasicAuth(accountname, password) {
       if (accountname !== undefined && accountname != null && accountname !== '') {
         this._username = accountname;
@@ -53,10 +53,10 @@ class NDEx {
       this._authType = undefined;
       // this._authStr = undefined;
     }
-  
+
     _httpGetOpenObj(URL) {
       let APIEndPointPrefix = this.host ;
-  
+
       return new Promise(function (resolve, reject) {
         axios({
           method: 'get',
@@ -72,9 +72,9 @@ class NDEx {
         );
       });
     }
-  
+
     // access endpoints that supports authentication
-  
+
     _getIdToken() {
       return this._authToken ? this._authToken : this._googleAuth.getAuthInstance().currentUser.get().getAuthResponse().id_token;
     }
@@ -85,24 +85,22 @@ class NDEx {
           password: this.password};
       } else if (this.authenticationType === 'g') {
         const idToken =  this._getIdToken();
-  
+
         if (config['headers'] === undefined) {config['headers'] = {};}
         config['headers']['Authorization'] = 'Bearer ' + idToken;
       }
     }
-  
-    _httpGetV3ProtectedObj(URL, parameters) {
-  
-      const v3baseURL = this.host.substring(0,this.host.lastIndexOf('v2')) + 'v3';
+
+    _httpGetProtectedObjAux (prefix, URL, parameters) {
 
       let config = {
         method: 'get',
         url: URL,
-        baseURL: v3baseURL
+        baseURL: prefix
       };
-  
+
       this._setAuthHeader(config);
-  
+
       if (parameters !== undefined) {
         config['params'] = parameters;
       }
@@ -116,52 +114,32 @@ class NDEx {
         (response) => {
           return reject(response);
         }
-        );
+      );
       });
-  
+    }
+
+    _httpGetV3ProtectedObj(URL, parameters) {
+      return this._httpGetProtectedObjAux(this._v3baseURL, URL, parameters);
     }
 
     _httpGetProtectedObj(URL, parameters) {
-  
-      let config = {
-        method: 'get',
-        url: URL,
-        baseURL: this.host
-      };
-  
-      this._setAuthHeader(config);
-  
-      if (parameters !== undefined) {
-        config['params'] = parameters;
-      }
-      return new Promise(function (resolve, reject) {
-        axios(config).then((response) => {
-          if (response.status === 200) {
-            return resolve(response.data);
-          }
-          return reject(response);
-        },
-        (response) => {
-          return reject(response);
-        }
-        );
-      });
-  
+      return this._httpGetProtectedObjAux(this.host, URL,parameters);
     }
-  
-    _httpPostProtectedObj(URL, parameters, data) {
+
+
+    _httpPostProtectedObjAux(prefix,  URL, parameters, data) {
       let config = {
         method: 'post',
         url: URL,
-        baseURL: this.host
+        baseURL: prefix
       };
-  
+
       this._setAuthHeader(config);
-  
+
       if (parameters !== undefined) {
         config['params'] = parameters;
       }
-  
+
       config.data = data;
       return new Promise(function (resolve, reject) {
         axios(config).then((response) => {
@@ -175,22 +153,31 @@ class NDEx {
         }
         );
       });
-  
     }
-  
+
+
+    _httpPostProtectedObj(URL, parameters, data) {
+      return this._httpPostProtectedObjAux(this.host,URL,parameters, data);
+    }
+
+    _httpPostV3ProtectedObj(URL, parameters, data) {
+      return this._httpPostProtectedObjAux(this._v3baseURL,URL,parameters, data);
+    }
+
+
     _httpPutObj(URL, parameters, data) {
       let config = {
         method: 'put',
         url: URL,
         baseURL: this.host
       };
-  
+
       this._setAuthHeader(config);
-  
+
       if (parameters !== undefined) {
         config['params'] = parameters;
       }
-  
+
       config.data = data;
       return new Promise(function (resolve, reject) {
         axios(config).then((response) => {
@@ -204,7 +191,7 @@ class NDEx {
         }
         );
       });
-  
+
     }
     _httpDeleteObj(URL, parameters) {
       let config = {
@@ -212,13 +199,13 @@ class NDEx {
         url: URL,
         baseURL: this.host
       };
-  
+
       this._setAuthHeader(config);
-  
+
       if (parameters !== undefined) {
         config['params'] = parameters;
       }
-  
+
       return new Promise(function (resolve, reject) {
         axios(config).then((response) => {
           if (response.status >= 200 && response.status <= 299) {
@@ -231,31 +218,31 @@ class NDEx {
         }
         );
       });
-  
+
     }
-  
+
     getStatus() {
       return this._httpGetOpenObj('admin/status');
     }
-  
+
     /* user functions */
-  
+
     getSignedInUser() {
       if (this._authType == null) {
         throw new Error('Authentication parameters are missing in NDEx client.');
       }
       return this._httpGetProtectedObj('user', {valid: true});
     }
-  
+
     getUser(uuid) {
       return this._httpGetProtectedObj('user/' + uuid);
     }
-  
+
     getAccountPageNetworks(offset, limit) {
       return new Promise((resolve, reject) => {
         this.getSignedInUser().then((user) => {
           let params = {};
-  
+
           if (offset !== undefined) {
             params.offset = offset;
           }
@@ -266,13 +253,13 @@ class NDEx {
             (networkArray) => {resolve(networkArray);}, (err) => {reject(err);}
           );
         }, (err) => {reject(err);});
-  
+
       });
     }
-  
+
     getAccountPageNetworksByUUID(uuid, offset, limit) {
       let params = {};
-  
+
       if (offset !== undefined) {
         params.offset = offset;
       }
@@ -281,29 +268,29 @@ class NDEx {
       }
       return this._httpGetProtectedObj('user/' + uuid + '/networksummary', params);
     }
-  
+
     /* group functions */
-  
+
     // return the UUID of the created group to the resolver
     createGroup(group) {
       return new Promise((resolve, reject)=> {
         this._httpPostProtectedObj('group', undefined, group).then(
           (response) => {
             let uuidr = response.split('/');
-  
+
             let uuid = uuidr[uuidr.length - 1];
-  
+
             return resolve(uuid);
           },
           (err) => {reject(err);}
         );
       });
     }
-  
+
     getGroup(uuid) {
       return this._httpGetProtectedObj('group/' + uuid);
     }
-  
+
     updateGroup(group) {
       return new Promise((resolve, reject)=> {
         this._httpPutObj('group/' + group.externalId, undefined, group).then(
@@ -314,76 +301,76 @@ class NDEx {
         );
       });
     }
-  
+
     deleteGroup(uuid) {
       return this._httpDeleteObj('group/' + uuid);
     }
-  
+
     /* network functions */
-  
+
     getRawNetwork(uuid, accessKey) {
-  
+
       let parameters = {};
-  
+
       if (accessKey !== undefined) {
         parameters = {accesskey: accessKey};
       }
-  
+
       return this._httpGetProtectedObj('network/' + uuid, parameters);
-  
+
     }
-  
+
     getCX2Network(uuid, accessKey) {
-  
+
       let parameters = {};
-  
+
       if (accessKey !== undefined) {
         parameters = {accesskey: accessKey};
       }
-  
+
       return this._httpGetV3ProtectedObj('networks/' + uuid, parameters);
-  
+
     }
 
     getNetworkSummary(uuid, accessKey) {
       let parameters = {};
-  
+
       if (accessKey !== undefined) {
         parameters = {accesskey: accessKey};
       }
-  
+
       return this._httpGetProtectedObj('network/' + uuid + '/summary', parameters);
     }
-  
+
     createNetworkFromRawCX(rawCX, parameters) {
       return new Promise((resolve, reject) =>{
         this._httpPostProtectedObj('network', parameters, rawCX).then(
           (response) => {
             let uuidr = response.split('/');
-  
+
             let uuid = uuidr[uuidr.length - 1];
-  
+
             return resolve(uuid);
           },
           (err) => {reject(err);}
         );
       });
     }
-  
+
     updateNetworkFromRawCX(uuid, rawcx) {
       return this._httpPutObj('network/' + uuid, undefined, rawcx);
     }
-  
+
     deleteNetwork(uuid) {
       return this._httpDeleteObj('network/' + uuid);
     }
-  
+
     /* task functions */
-  
+
     /* search functions */
     searchUsers(searchTerms, start, size) {
       let params = {};
-  
+
       if (start !== undefined) {
         params.start = start;
       }
@@ -391,14 +378,14 @@ class NDEx {
         params.limit = size;
       }
       let data = {searchString: searchTerms};
-  
+
       return this._httpPostProtectedObj('search/user', params, data);
-  
+
     }
-  
+
     searchGroups(searchTerms, start, size) {
       let params = {};
-  
+
       if (start !== undefined) {
         params.start = start;
       }
@@ -406,23 +393,23 @@ class NDEx {
         params.limit = size;
       }
       let data = {searchString: searchTerms};
-  
+
       return this._httpPostProtectedObj('search/group', params, data);
-  
+
     }
-  
+
     searchNetworks(searchTerms, start, size, optionalParameters) {
       let params = {};
-  
+
       if (start !== undefined) {
         params.start = start;
       }
       if (size !== undefined) {
         params.limit = size;
       }
-  
+
       let data = {searchString: searchTerms};
-  
+
       if (optionalParameters !== undefined) {
         if (optionalParameters.permission !== undefined) {
           data.permission = optionalParameters.permission;
@@ -434,18 +421,18 @@ class NDEx {
           data.accountName = optionalParameters.accountName;
         }
       }
-  
+
       return this._httpPostProtectedObj('search/network', params, data);
     }
-  
+
     neighborhoodQuery(uuid, searchTerms, saveResult, parameters) {
       let params = {};
-  
+
       if (saveResult !== undefined && saveResult === true) {params.save = true;}
-  
+
       let data = {searchString: searchTerms,
         searchDepth: 1};
-  
+
       if (parameters !== undefined) {
         if (parameters.searchDepth !== undefined) {
           data.searchDepth = parameters.searchDepth;
@@ -461,16 +448,16 @@ class NDEx {
         }
       }
       return this._httpPostProtectedObj('search/network/' + uuid + '/query', params, data);
-  
+
     }
-  
+
     interConnectQuery(uuid, searchTerms, saveResult, parameters) {
       let params = {};
-  
+
       if (saveResult !== undefined && saveResult === true) {params.save = true;}
-  
+
       let data = {searchString: searchTerms};
-  
+
       if (parameters !== undefined) {
         if (parameters.edgeLimit !== undefined) {
           data.edgeLimit = parameters.edgeLimit;
@@ -480,36 +467,105 @@ class NDEx {
         }
       }
       return this._httpPostProtectedObj('search/network/' + uuid + '/interconnectquery', params, data);
-  
+
     }
-  
+
     /* batch functions */
     getUsersByUUIDs(uuidList) {
       return this._httpPostProtectedObj('batch/user', undefined, uuidList);
     }
-  
+
     getGroupsByUUIDs(uuidList) {
       return this._httpPostProtectedObj('batch/group', undefined, uuidList);
     }
-  
+
     getNetworkSummariesByUUIDs(uuidList, accessKey) {
       let parameter = accessKey === undefined ? undefined :
         {accesskey: accessKey};
-  
+
       return this._httpPostProtectedObj('batch/network/summary', parameter, uuidList);
     }
-  
+
     getNetworkPermissionsByUUIDs(uuidList) {
       return this._httpPostProtectedObj('batch/network/permission', undefined, uuidList);
     }
-  
+
     exportNetworks(exportJob) {
       return this._httpPostProtectedObj('batch/network/export', undefined, exportJob);
     }
-  
+
     /* network set functions */
-  
+
     /* undocumented functions. Might be changed ... */
-  
+
+    // new v3 functions
+    getRandomEdges(uuid, limit, accessKey ) {
+
+      if ( limit <=0)
+        throw new Error("Value of parameter limit has to be greater than 0.");
+
+      let parameters = {
+        size: limit,
+        method: "random"
+      };
+
+      if (accessKey !== undefined) {
+      parameters = {accesskey: accessKey};
+      }
+
+      return this._httpGetV3ProtectedObj('networks/' + uuid + '/edges', parameters);
+    }
+
+    getAspectElements(uuid, aspectName, limit, accessKey ) {
+
+      let parameters = {
+      };
+
+      if ( limit !== undefined) {
+        parameters = {
+          size: limit
+        };
+      }
+
+      if (accessKey !== undefined) {
+        parameters ['accesskey'] =accessKey;
+      }
+
+      return this._httpGetV3ProtectedObj('networks/' + uuid + '/' + aspectName, parameters);
+    }
+
+  getFilteredEdges(uuid, columnName, valueString, operator, limit, order, format, accessKey ) {
+
+    let parameters = {
+    };
+
+
+    if ( limit !== undefined) {
+      parameters = {
+        size: limit
+      };
+    }
+
+    if ( order !== undefined) {
+      parameters['order'] = order;
+    }
+
+    if (accessKey !== undefined) {
+      parameters ['accesskey'] = accessKey;
+    }
+
+    if ( format !== undefined ) {
+      parameters['format'] = format;
+    }
+
+    let data = {
+      "name": columnName,
+      "value": valueString,
+      "operator": operator
+    };
+
+    return this._httpPostV3ProtectedObj('/search/networks/' + uuid + '/edges', parameters, data);
   }
+
+}
   module.exports = { NDEx };
