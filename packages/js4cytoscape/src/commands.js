956589869941d2b12b77af3c9466ec019eadfd4f
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-06-20 20:47:23 -0700

    implement cyrestAPI

--- packages/js4cytoscape/src/commands.js
+++ packages/js4cytoscape/src/commands.js
@@ -1,9 +1,38 @@
-let CYREST_BASE_URL = 'http://127.0.0.1:1234';
+const defaultBaseUrl = 'http://127.0.0.1:1234/v1';
 
 
-async function getCurrent(){
-    const geturl=CYREST_BASE_URL +"/v1/networks/currentNetwork";
-    let response = await fetch(geturl);
-    let data = await response.json();
-    console.log(data);
+// I. CyREST API functions
+async function cyrestGet(operation = '', parameters = '', baseUrl = defaultBaseUrl) {
+    let qurl = baseUrl.concat('/', operation);
+    if (parameters != '') {
+        const qparameters = parameters;
+        qurl = qurl.concat('?', qparameters);
+    }
+    const res = await fetch(qurl, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+      const json = await res.json();
+      console.log(JSON.stringify(json));
+}
+
+
+async function cyrestDelete(operation = '', parameters = '', baseUrl = defaultBaseUrl) {
+    let qurl = baseUrl.concat('/', operation);
+    if (parameters != '') {
+        const qparameters = parameters;
+        qurl = qurl.concat('?', qparameters);
+    }
+    const res = await fetch(qurl, {
+        method: 'DELETE',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+      const json = await res.json();
+      console.log(JSON.stringify(json));
 }
