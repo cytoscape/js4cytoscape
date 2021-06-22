7e2984b13ea183f301af556f3cab8327e1ca4cbd
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-06-21 22:26:16 -0700

    build library

--- packages/js4cytoscape/dist/main.js
+++ packages/js4cytoscape/dist/main.js
@@ -75,3 +75,10 @@ export async function cyrestPUT(operation = '', parameters = '', body = '', base
       const json = await res.json();
       console.log(JSON.stringify(json));
 }
+
+let defaultBaseUrl = 'http://127.0.0.1:1234/v1';
+
+
+export async function deleteAllNetworks(baseUrl = defaultBaseUrl) {
+    console.log("All networks are deleted.")
+}
