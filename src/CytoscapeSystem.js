549a7cb4e6694c6ac72fe74f77a7b89574f17259
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-06-22 15:07:05 -0700

    build library

--- src/CytoscapeSystem.js
+++ src/CytoscapeSystem.js
@@ -2,3 +2,9 @@ async function cytoscapeVersionInfo(baseUrl = defaultBaseUrl) {
     let res = cyrestGET('version', baseUrl = baseUrl);
     console.log(res)
 }
+
+
+async function cytoscapeMemoryStatus(baseUrl = defaultBaseUrl) {
+    let res = cyrestGET('', baseUrl = baseUrl);
+    console.log(res);
+}
