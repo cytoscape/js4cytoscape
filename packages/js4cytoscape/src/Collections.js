606f7852cd1ba6023c08298975e6df8a09b3da4e
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-06-22 22:37:09 -0700

    build library

--- packages/js4cytoscape/src/Collections.js
+++
@@ -1,5 +0,0 @@
-async function getCollectionList(baseUrl = defaultBaseUrl) {
-    let res = cyrestGET('collections', baseUrl = baseUrl);
-    const apiVersion = res.then(data => { console.log(data['apiVersion']) });
-    const cytoscapeVersion = res.then(data => { console.log(data['cytoscapeVersion']) });
-}
