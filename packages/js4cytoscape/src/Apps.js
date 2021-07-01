e8ffeb25b74371bd402bfdd76ff0651cb36fac9f
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-06-30 22:30:15 -0700

    implement commands API

--- packages/js4cytoscape/src/Apps.js
+++ packages/js4cytoscape/src/Apps.js
@@ -1,3 +1,23 @@
 async function getAvailableApps(baseUrl = defaultBaseUrl) {
     let res = commandsGET('apps list available', baseUrl=baseUrl);
 }
+
+
+async function getInstalledApps(baseUrl = defaultBaseUrl) {
+    let res = commandsGET('apps list installed', baseUrl=baseUrl);
+}
+
+
+async function getDisabledApps(baseUrl = defaultBaseUrl) {
+    let res = commandsGET('apps list disabled', baseUrl=baseUrl);
+}
+
+
+async function getAppUpdates(baseUrl = defaultBaseUrl) {
+    let res = commandsGET('apps list updates', baseUrl=baseUrl);
+}
+
+
+async function openAppStore(app, baseUrl = defaultBaseUrl) {
+    let res = commandsGET('apps open appstore app=' + "'" + app + "'", baseUrl=baseUrl);
+}
