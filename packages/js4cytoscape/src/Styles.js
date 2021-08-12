5707b3d51716c1df0e5e75054de521160ae21216
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-08-11 21:51:26 -0700

    implement style functions

--- packages/js4cytoscape/src/Styles.js
+++ packages/js4cytoscape/src/Styles.js
@@ -12,3 +12,24 @@ async function getCurrentStyle(network = null, baseUrl = defaultBaseUrl){
     console.log(res['title']);
     return res['title'];
 }
+
+
+async function getArrowShapes(baseUrl = defaultBaseUrl){
+    let res = await cyrestGET("styles/visualproperties/EDGE_TARGET_ARROW_SHAPE/values", baseUrl);
+    console.log(res['values']);
+    return res['values'];
+}
+
+
+async function getLineStyles(baseUrl = defaultBaseUrl){
+    let res = await cyrestGET("styles/visualproperties/EDGE_LINE_TYPE/values", baseUrl);
+    console.log(res['values']);
+    return res['values'];
+}
+
+
+async function getNodeShapes(baseUrl = defaultBaseUrl){
+    let res = await cyrestGET("styles/visualproperties/NODE_SHAPE/values", baseUrl);
+    console.log(res['values']);
+    return res['values'];
+}
