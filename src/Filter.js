269f4bfdf8b9a51aad3eb623bc3e4a10519b361a
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-10-21 00:55:17 -0700

    import js4cytoscape

--- src/Filter.js
+++
@@ -1,4 +0,0 @@
-async function getFilterList(baseUrl = defaultBaseUrl) {
-    let filterList = commandsPOST('filter list', baseUrl=baseUrl);
-    let res = filterList.then(data => { console.log('Filter list: ' + JSON.stringify(JSON.parse(data)['data'])) });
-}
--- src/Tools.js
+++
@@ -1,4 +0,0 @@
-async function analyzeNetwork(directed = false, baseUrl = defaultBaseUrl) {
-    let analyze = commandsPOST('analyzer analyze directed=' + directed, baseUrl=baseUrl);
-    let res = analyze.then(data => { console.log('Analyze network: ' + JSON.stringify(JSON.parse(data)['data']))});
-}
