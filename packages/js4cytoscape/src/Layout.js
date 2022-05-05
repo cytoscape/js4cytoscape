e6f85ad4597ace64a73cfcab12a0a058ff48d280
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2022-05-04 23:56:45 -0700

    add deleteSelectedEdges

--- packages/js4cytoscape/src/Layout.js
+++ packages/js4cytoscape/src/Layout.js
@@ -4,7 +4,7 @@ async function layoutNetwork(layoutName = null, network = null, baseUrl = defaul
       let cmd = 'layout apply preferred networkSelected="SUID:' + suid + '"';
       let res = commandsPOST(cmd, baseUrl=baseUrl);
     } else {
-      let cmd = 'layout' + layoutName + 'network="SUID:' + suid + '"';
+      let cmd = 'layout ' + layoutName + ' network="SUID:' + suid + '"';
       let res = commandsPOST(cmd, baseUrl=baseUrl);
   }
 }
