88106bf42cfb5e0b616332b18a0276a49fda451e
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2022-05-04 23:44:16 -0700

    add deleteSelectedEdges

---
+++ packages/js4cytoscape/src/NetworkSelection.js
@@ -0,0 +1,5 @@
+async function deleteSelectedEdges(network = null, baseUrl = defaultBaseUrl){
+    let suid = await getNetworkSuid(network, baseUrl);
+    let cmd = 'network delete edgeList=selected network=SUID:' + suid + '"';
+    let res = commandsPOST(cmd, baseUrl=baseUrl);
+}
