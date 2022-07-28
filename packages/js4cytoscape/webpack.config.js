b7c9c025923b9af2c2cf5041d862d6becbb0f968
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2022-07-28 16:21:21 -0700

    Add AppManager.js

--- packages/js4cytoscape/webpack.config.js
+++ packages/js4cytoscape/webpack.config.js
@@ -3,7 +3,7 @@ const ConcatPlugin = require('webpack-concat-plugin');
 
 module.exports = {
   mode: "development",
-  entry: ['./src/Commands.js', './src/Networks.js', './src/CytoscapeSystem.js', './src/Collections.js', './src/CyNDEx.js', './src/js4cytoscape-utils.js', './src/Apps.js', './src/Filter.js', './src/Tools.js','./src/NetworkViews.js','./src/Session.js','./src/Styles.js','./src/Layout.js','./src/NetworkSelection.js'], 
+  entry: ['./src/Commands.js', './src/Networks.js', './src/CytoscapeSystem.js', './src/Collections.js', './src/CyNDEx.js', './src/js4cytoscape-utils.js', './src/Apps.js', './src/Filter.js', './src/Tools.js','./src/NetworkViews.js','./src/Session.js','./src/Styles.js','./src/Layout.js','./src/NetworkSelection.js','./src/AppManager.js'],
   plugins: [
     new ConcatPlugin({
       uglify: true,
@@ -11,7 +11,7 @@ module.exports = {
       name: 'result',
       outputPath: '',
       fileName: 'main.js',
-      filesToConcat: ['./src/Commands.js', './src/Networks.js', './src/CytoscapeSystem.js', './src/Collections.js', './src/CyNDEx.js', './src/js4cytoscape-utils.js', './src/Apps.js', './src/Filter.js', './src/Tools.js','./src/NetworkViews.js','./src/Session.js','./src/Styles.js','./src/Layout.js','./src/NetworkSelection.js'],
+      filesToConcat: ['./src/Commands.js', './src/Networks.js', './src/CytoscapeSystem.js', './src/Collections.js', './src/CyNDEx.js', './src/js4cytoscape-utils.js', './src/Apps.js', './src/Filter.js', './src/Tools.js','./src/NetworkViews.js','./src/Session.js','./src/Styles.js','./src/Layout.js','./src/NetworkSelection.js','./src/AppManager.js'],
       attributes: {
           async: true
       }
