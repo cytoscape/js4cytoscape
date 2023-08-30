1ca6a6fb0f0f775c7105ee482df2d62a029a836b
yihangx <yihang.xin@gladstone.ucsf.edu>
2023-08-30 01:53:21 -0700

    CYTOSCAPE-13103

--- packages/js4cytoscape/src/Tools.js
+++ packages/js4cytoscape/src/Tools.js
@@ -7,3 +7,14 @@ async function analyzeNetwork(directed = false, baseUrl = defaultBaseUrl) {
     let analyze = commandsPOST('analyzer analyze directed=' + directed, baseUrl=baseUrl);
     let res = analyze.then(data => { console.log('Analyze network: ' + JSON.stringify(JSON.parse(data)['data']))});
 }
+
+
+/**
+ * Import wikipathway networks into Cytoscape.
+ *
+ * @param {String} wikipathway ID
+ */
+async function wikipathwayImport(wikipathwayId, baseUrl = defaultBaseUrl) {
+    let importAsPathway = commandsGET('wikipathways import-as-pathway id=' + wikipathwayId, baseUrl=baseUrl);
+    let res = importAsPathway.then(data => { console.log('Import as pathway: ' + JSON.stringify(JSON.parse(data)['data']))});
+}
