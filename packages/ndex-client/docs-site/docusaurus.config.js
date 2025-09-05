dd6eba3acce84360f416bfb23fb81d390a5ef90a
jingjingbic <jingjingbic@gmail.com>
2025-09-05 13:31:02 -0700

    Add automated documentation deployment workflow
      - Auto-detects packages with TSDoc/Docusaurus setup
      - Builds TypeDoc API docs first, then Docusaurus site
      - Creates unified landing page for all packages
      - Supports monorepo architecture with multiple packages

--- packages/ndex-client/docs-site/docusaurus.config.js
+++ packages/ndex-client/docs-site/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://js4cytoscape.github.io',
+  url: 'https://cytoscape.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ndex-client/',
+  baseUrl: '/js4cytoscape/ndex-client/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
