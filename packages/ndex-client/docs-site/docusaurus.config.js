1b7d0a4964dad9fdd9530e18f4dbe801f677d409
jingjingbic <jingjingbic@gmail.com>
2025-09-05 15:57:26 -0700

    Try to fix the path issue when the gh page is deployed.

--- packages/ndex-client/docs-site/docusaurus.config.js
+++ packages/ndex-client/docs-site/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://cytoscape.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/js4cytoscape/ndex-client/',
+  baseUrl: '/js4cytoscape/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
